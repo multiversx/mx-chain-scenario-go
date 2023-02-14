e69edcc8d6c48648e0805d711b4a7960491c393f
Andrei Marinica <andrei.marinica@elrond.com>
2023-02-14 20:18:20 +0200

    renamed scenario runner

--- controller/scenarioRunner.go
+++ controller/scenarioRunner.go
@@ -6,27 +6,27 @@ import (
 	mj "github.com/multiversx/mx-chain-scenario-go/model"
 )
 
-// ScenarioExecutor describes a component that can run a VM scenario.
-type ScenarioExecutor interface {
+// ScenarioRunner describes a component that can run a VM scenario.
+type ScenarioRunner interface {
 	// Reset clears state/world.
 	Reset()
 
-	// ExecuteScenario executes the scenario and checks if it passed. Failure is signaled by returning an error.
+	// RunScenario executes the scenario and checks if it passed. Failure is signaled by returning an error.
 	// The FileResolver helps with resolving external steps.
 	// TODO: group into a "execution context" param.
-	ExecuteScenario(*mj.Scenario, fr.FileResolver) error
+	RunScenario(*mj.Scenario, fr.FileResolver) error
 }
 
-// ScenarioRunner is a component that can run json scenarios, using a provided executor.
-type ScenarioRunner struct {
-	Executor    ScenarioExecutor
+// ScenarioController is a component that can run json scenarios, using a provided executor.
+type ScenarioController struct {
+	Executor    ScenarioRunner
 	RunsNewTest bool
 	Parser      mjparse.Parser
 }
 
-// NewScenarioRunner creates new ScenarioRunner instance.
-func NewScenarioRunner(executor ScenarioExecutor, fileResolver fr.FileResolver) *ScenarioRunner {
-	return &ScenarioRunner{
+// NewScenarioController creates new ScenarioController instance.
+func NewScenarioController(executor ScenarioRunner, fileResolver fr.FileResolver) *ScenarioController {
+	return &ScenarioController{
 		Executor: executor,
 		Parser:   mjparse.NewParser(fileResolver),
 	}
