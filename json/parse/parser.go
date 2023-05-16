8a2d0ef5af6e86a5ff8875a2043f9670724e1512
Andrei Marinica <andrei.marinica@elrond.com>
2023-05-16 16:54:12 +0300

    allow single values in check value list

--- json/parse/parser.go
+++ json/parse/parser.go
@@ -7,10 +7,11 @@ import (
 
 // Parser performs parsing of both json tests (older) and scenarios (new).
 type Parser struct {
-	ExprInterpreter            ei.ExprInterpreter
-	AllowEsdtTxLegacySyntax    bool
-	AllowEsdtLegacySetSyntax   bool
-	AllowEsdtLegacyCheckSyntax bool
+	ExprInterpreter                  ei.ExprInterpreter
+	AllowEsdtTxLegacySyntax          bool
+	AllowEsdtLegacySetSyntax         bool
+	AllowEsdtLegacyCheckSyntax       bool
+	AllowSingleValueInCheckValueList bool
 }
 
 // NewParser provides a new Parser instance.
@@ -19,8 +20,9 @@ func NewParser(fileResolver fr.FileResolver) Parser {
 		ExprInterpreter: ei.ExprInterpreter{
 			FileResolver: fileResolver,
 		},
-		AllowEsdtTxLegacySyntax:    true,
-		AllowEsdtLegacySetSyntax:   true,
-		AllowEsdtLegacyCheckSyntax: true,
+		AllowEsdtTxLegacySyntax:          true,
+		AllowEsdtLegacySetSyntax:         true,
+		AllowEsdtLegacyCheckSyntax:       true,
+		AllowSingleValueInCheckValueList: true,
 	}
 }
