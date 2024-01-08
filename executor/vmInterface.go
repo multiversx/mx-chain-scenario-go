b66121994530ed1529d546be899078fb00f3fcae
Andrei Marinica <andrei.marinica@multiversx.com>
2024-01-08 10:34:47 +0200

    docs

--- executor/vmInterface.go
+++ executor/vmInterface.go
@@ -16,8 +16,14 @@ type VMInterface interface {
 	GetGasTrace() map[string]map[string][]uint64
 }
 
+// ScenarioVMBuilder defines VM initialization.
+//
+// The VM is not passed directly to the scenario executor because the scenario can override the gas schedule,
+// which is required during initialization.
 type ScenarioVMBuilder interface {
+	// GasScheduleMapFromScenarios converts the gas schedule name from a scenario into an actual gas map.
 	GasScheduleMapFromScenarios(scenGasSchedule mj.GasSchedule) (worldmock.GasScheduleMap, error)
 
+	// NewVM creates the execution VM host with references to the world mock and gas schedule.
 	NewVM(world *worldmock.MockWorld, gasSchedule map[string]map[string]uint64) (VMInterface, error)
 }
